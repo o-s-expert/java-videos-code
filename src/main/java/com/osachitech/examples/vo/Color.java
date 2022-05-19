02bd4c1d363fb35b0e3ba68179dffa8651d09e96
Otavio Santana <otaviopolianasantana@gmail.com>
2022-05-19 20:15:42 +0100

    Define color as VO
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/main/java/com/osachitech/examples/vo/Color.java
@@ -0,0 +1,20 @@
+package com.osachitech.examples.vo;
+
+import java.util.function.Supplier;
+
+public enum Color implements Supplier<String> {
+
+    BLUE("#00FFFF"), WHITE("#FFFFFF"),
+    BLACK("#000000"), GREEN("#008000");
+
+    private final String value;
+
+    Color(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String get() {
+        return value;
+    }
+}
