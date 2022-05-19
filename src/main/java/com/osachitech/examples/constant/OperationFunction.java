7ac52431d678e1b6d0c33e3a75166941ac013d33
Otavio Santana <otaviopolianasantana@gmail.com>
2022-05-19 19:59:12 +0100

    simplify it with function
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/main/java/com/osachitech/examples/constant/OperationFunction.java
@@ -0,0 +1,21 @@
+package com.osachitech.examples.constant;
+
+import java.util.function.BiFunction;
+
+public enum OperationFunction {
+
+    PLUS ((a,b) -> a + b),
+    MINUS ((a,b) -> a - b),
+    TIMES ((a,b) -> a * b),
+    DIVIDE ((a,b) -> a / b);
+
+    private final BiFunction<Double,Double,Double> operation ;
+
+    OperationFunction(BiFunction<Double,Double,Double> operation) {
+        this.operation = operation;
+    }
+
+    public Double compute(Double x, Double y) {
+        return operation.apply(x,y);
+    }
+}
