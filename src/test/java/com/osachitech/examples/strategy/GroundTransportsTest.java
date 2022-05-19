71916cdbacd233ad13fab6fe69cf7bde6a1acd53
Otavio Santana <otaviopolianasantana@gmail.com>
2022-05-19 22:34:43 +0100

    create test to move
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/com/osachitech/examples/strategy/GroundTransportsTest.java
+++ src/test/java/com/osachitech/examples/strategy/GroundTransportsTest.java
@@ -1,7 +1,13 @@
 package com.osachitech.examples.strategy;
 
+import com.osachitech.examples.vo.Currencies;
+import org.javamoney.moneta.Money;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import javax.money.Monetary;
+import javax.money.MonetaryAmount;
+import java.time.Duration;
 
 import static com.osachitech.examples.strategy.GroundTransports.BIKE;
 import static com.osachitech.examples.strategy.GroundTransports.BUS;
@@ -22,6 +28,15 @@ class GroundTransportsTest {
         Assertions.assertThrows(IllegalArgumentException.class, () -> BIKE.move(0));
         Assertions.assertThrows(IllegalArgumentException.class, () -> TAXI.move(0));
         Assertions.assertThrows(IllegalArgumentException.class, () -> BUS.move(0));
+    }
+
+    @Test
+    public void shouldMoveOneHundredKM() {
+        MonetaryAmount free = Money.of(0, Currencies.EURO.get());
+
+        Trajectory move = BIKE.move(100);
+        Assertions.assertEquals(Duration.ofHours(5L), move.getDuration());
+        Assertions.assertEquals(free, move.getDuration());
     }
 
 }
