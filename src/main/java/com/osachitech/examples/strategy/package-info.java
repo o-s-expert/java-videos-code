a8b013ffd398e4268ef3e87b1c3abee4597d330c
Otavio Santana <otaviopolianasantana@gmail.com>
2022-05-19 21:31:34 +0100

    Create strategy pattern domain
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/main/java/com/osachitech/examples/strategy/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * In Strategy pattern, a class behavior or its algorithm can be changed at run time.
+ * This type of design pattern comes under behavior pattern.
+ */
+package com.osachitech.examples.strategy;
