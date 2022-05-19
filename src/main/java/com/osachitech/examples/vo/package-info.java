8bfba654d18bd8e7539bd593d0b6ee2675db8fa3
Otavio Santana <otaviopolianasantana@gmail.com>
2022-05-19 20:11:56 +0100

    Create VO domain
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/main/java/com/osachitech/examples/vo/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Enum also helps with Value Object.
+ *
+ * In DDD, value objects differ from entities by
+ * lacking the concept of identity.
+ * We do not care who they are but rather what they are.
+ * They are defined by their attributes and should be immutable.
+ */
+package com.osachitech.examples.vo;
