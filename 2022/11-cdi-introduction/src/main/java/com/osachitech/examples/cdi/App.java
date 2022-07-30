db559502450b4ed1dd25f2d87d636e4a9d9b2bfe
Otavio Santana <otaviopolianasantana@gmail.com>
2022-07-30 10:57:33 +0100

    remove extra space on lazybean
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2022/11-cdi-introduction/src/main/java/com/osachitech/examples/cdi/App.java
+++ 2022/11-cdi-introduction/src/main/java/com/osachitech/examples/cdi/App.java
@@ -5,7 +5,6 @@ import jakarta.enterprise.inject.se.SeContainer;
 import jakarta.enterprise.inject.se.SeContainerInitializer;
 
 public class App {
-
     public static void main(String[] args) {
 
         try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
