747a9cea681986bbbf22d8cbd4d9413b20832165
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-14 08:48:12 -0300

    update bookcache validation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2022/12-microstream/src/main/java/org/a4j/workshop/helidon/microstream/BookCache.java
+++ 2022/12-microstream/src/main/java/org/a4j/workshop/helidon/microstream/BookCache.java
@@ -6,6 +6,7 @@ import one.microstream.integrations.cdi.types.cache.StorageCache;
 import javax.cache.Cache;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.util.Objects;
 import java.util.Optional;
 
 @ApplicationScoped
@@ -17,11 +18,13 @@ public class BookCache {
 
     @Store
     public void add(Book book) {
+        Objects.requireNonNull(book, "book is required");
         this.cache.put(book.getId(), book);
     }
 
     public Optional<Book> getById(String id) {
-        return Optional.ofNullable(this.)
+        Objects.requireNonNull(id, "id is required");
+        return Optional.ofNullable(this.cache.get(id));
     }
 
 }
