186bc8b77aa039fbca34b8803965ff3b777938d3
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-25 11:23:36 +0000

    update pereson
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/02-record/src/main/java/expert/os/examples/App.java
+++ 2023/02-record/src/main/java/expert/os/examples/App.java
@@ -1,10 +1,16 @@
 package expert.os.examples;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Person person = new Person("Otavio", LocalDate.of(1988,
+                Month.JANUARY, 9), "Salvador");
+
+        System.out.println("The person: " + person);
     }
 }
--- 2023/02-record/src/main/java/expert/os/examples/Person.java
+++ 2023/02-record/src/main/java/expert/os/examples/Person.java
@@ -1,60 +1,7 @@
 package expert.os.examples;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
-public class Person {
+public record Person(String name, LocalDate birthday, String city) {
 
-    private final String name;
-
-    private final LocalDate birthday;
-
-    private final String city;
-
-    public Person(String name, LocalDate birthday, String city) {
-        this.name = name;
-        this.birthday = birthday;
-        this.city = city;
-    }
-
-
-    public String name() {
-        return name;
-    }
-
-    public LocalDate birthday() {
-        return birthday;
-    }
-
-    public String city() {
-        return city;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Person person = (Person) o;
-        return Objects.equals(name, person.name)
-                && Objects.equals(birthday, person.birthday)
-                && Objects.equals(city, person.city);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, birthday, city);
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", birthday=" + birthday +
-                ", city='" + city + '\'' +
-                '}';
-    }
 }
