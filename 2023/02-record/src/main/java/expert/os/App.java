7999b53c7c33a94d0757a98dfe48a514390f1023
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-25 10:58:15 +0000

    create record sample project
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ 2023/02-record/src/main/java/expert/os/App.java
@@ -0,0 +1,10 @@
+package expert.os;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ 2023/02-record/src/test/java/expert/os/AppTest.java
@@ -0,0 +1,20 @@
+package expert.os;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldTest() {
+    }
+
+    @Test
+    public void shouldTestB() {
+    }
+}
