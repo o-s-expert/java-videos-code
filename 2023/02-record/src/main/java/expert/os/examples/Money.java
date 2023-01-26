e2ec3f065cdbaa474af6d342b20618427b931a53
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-26 10:48:03 +0000

    create money as value objet
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/02-record/src/main/java/expert/os/examples/Money.java
+++ 2023/02-record/src/main/java/expert/os/examples/Money.java
@@ -1,2 +1,17 @@
-package expert.os.examples;public record Money() {
+package expert.os.examples;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Objects;
+
+public record Money(Currency currency, BigDecimal value) {
+
+    Money add(Money money) {
+        Objects.requireNonNull(money, "Money is required");
+        if (currency.equals(money.currency)) {
+            BigDecimal result = this.value.add(money.value);
+            return new Money(currency, result);
+        }
+        throw new IllegalStateException("You cannot sum money with different currencies");
+    }
 }
