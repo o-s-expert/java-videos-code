7ec82d8f5369124ad815a3dfc2ce5d06a10f9f7c
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-26 11:50:42 +0000

    add validatoon on Order
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/02-record/src/main/java/expert/os/examples/Delivered.java
+++ 2023/02-record/src/main/java/expert/os/examples/Delivered.java
@@ -2,8 +2,13 @@ package expert.os.examples;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public record Delivered(List<String> products) implements Order {
+
+    public Delivered {
+        Objects.requireNonNull(products, "products is required");
+    }
     @Override
     public Order next() {
         return new Received(products);
--- 2023/02-record/src/main/java/expert/os/examples/Ordered.java
+++ 2023/02-record/src/main/java/expert/os/examples/Ordered.java
@@ -2,8 +2,13 @@ package expert.os.examples;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public record Ordered(List<String> products) implements Order {
+
+    public Ordered {
+        Objects.requireNonNull(products, "products is required");
+    }
     @Override
     public Order next() {
         return new Delivered(products);
