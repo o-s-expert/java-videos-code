5b78c9e56307e93d5e7506fe3d7ca85b6e74c29e
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-28 12:26:56 +0000

    remove unused codes
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/03-java-doc-test/src/main/java/expert/os/examples/App.java
+++ 2023/03-java-doc-test/src/main/java/expert/os/examples/App.java
@@ -1,16 +1,9 @@
 package expert.os.examples;
 
-import java.time.LocalDate;
-import java.time.Month;
-
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        Person person = new Person("Otavio", LocalDate.of(1988,
-                Month.JANUARY, 9), "Salvador");
-
-        System.out.println("The person: " + person);
     }
 }
--- 2023/03-java-doc-test/src/main/java/expert/os/examples/Person.java
+++
@@ -1,7 +0,0 @@
-package expert.os.examples;
-
-import java.time.LocalDate;
-
-public record Person(String name, LocalDate birthday, String city) {
-
-}
