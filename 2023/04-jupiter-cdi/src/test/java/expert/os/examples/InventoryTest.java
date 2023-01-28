967df8e81a8c26bd28b941a840f85bc5ade1869b
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-28 17:26:07 +0000

    define payment mock
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/04-jupiter-cdi/src/test/java/expert/os/examples/InventoryTest.java
+++ 2023/04-jupiter-cdi/src/test/java/expert/os/examples/InventoryTest.java
@@ -1,8 +1,10 @@
 package expert.os.examples;
 
+import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Inject;
 import org.jboss.weld.junit5.auto.AddPackages;
 import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.jboss.weld.junit5.auto.ExcludeBean;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,6 +15,10 @@ class InventoryTest {
 
     @Inject
     private Inventory inventory;
+
+    @Produces
+    @ExcludeBean
+    private Payment payment = new PaymentMock();
 
     @Test
     public void shouldBy() {
