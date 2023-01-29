f5696d35543bbe5cd89ec40e6827ae024a8a07db
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-29 17:49:56 +0000

    create test scene 01
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ 2023/05-reflections/src/test/java/expert/os/examples/mapping/MapperTest.java
@@ -0,0 +1,35 @@
+package expert.os.examples.mapping;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MapperTest {
+
+
+    private Mapper mapper;
+
+    @BeforeEach
+    public void setUp() {
+        this.mapper = new ReflectionMapper();
+    }
+
+    @Test
+    public void shouldConvertToMap() {
+        Pet ada = Pet.of("Ada", 8);
+        Map<String, Object> map = mapper.toMap(ada);
+
+        Assertions.assertThat(map)
+                .isNotNull()
+                .isNotEmpty()
+                .containsKeys("_entity", "name", "age")
+                .containsEntry("name", "Ada")
+                .containsEntry("age", 8)
+                .containsEntry("_entity", Pet.class.getName());
+
+    }
+}
