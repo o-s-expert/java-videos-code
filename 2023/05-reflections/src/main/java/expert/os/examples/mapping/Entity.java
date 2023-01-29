cecf2eb738bb682bd3567d96782f7ced74aee457
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-29 17:27:16 +0000

    create annotaions
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/05-reflections/src/main/java/expert/os/examples/mapping/Entity.java
+++ 2023/05-reflections/src/main/java/expert/os/examples/mapping/Entity.java
@@ -1,11 +1,4 @@
 package expert.os.examples.mapping;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
 public @interface Entity {
 }
--- 2023/05-reflections/src/main/java/expert/os/examples/mapping/Mapper.java
+++ 2023/05-reflections/src/main/java/expert/os/examples/mapping/Mapper.java
@@ -4,6 +4,13 @@ import java.util.Map;
 
 public interface Mapper {
 
+    /**
+     *
+     * @param entity
+     * @param <T>
+     * @return
+     * @throws NullPointerException when the entity is null
+     */
     <T> Map<String, Object> toMap(T entity);
 
     <T> T toEntity(Map<String, Object> map);
