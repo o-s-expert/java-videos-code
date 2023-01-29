a6cb59e285119a63c5df547307b6161a0446f275
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-29 17:45:05 +0000

    create reflection implementation scene
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/05-reflections/src/main/java/expert/os/examples/mapping/ReflectionMapper.java
+++ 2023/05-reflections/src/main/java/expert/os/examples/mapping/ReflectionMapper.java
@@ -1,5 +1,6 @@
 package expert.os.examples.mapping;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,7 +8,10 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Predicate;
 
-public class ReflectionMapper implements Mapper{
+public class ReflectionMapper implements Mapper {
+
+    public static final String ENTITY_ENTRY = "_entity";
+
     @Override
     public <T> Map<String, Object> toMap(T entity) {
         Objects.requireNonNull(entity, "entity is required");
@@ -15,13 +19,13 @@ public class ReflectionMapper implements Mapper{
         Map<String, Object> map = new HashMap<>();
 
         Class<?> type = entity.getClass();
-        map.put("_entity", type.getName());
+        map.put(ENTITY_ENTRY, type.getName());
 
         for (Field field : type.getDeclaredFields()) {
             field.setAccessible(true);
 
             Optional<Column> column = Optional.ofNullable(field.getAnnotation(Column.class));
-            if(column.isPresent()) {
+            if (column.isPresent()) {
                 String key = column.map(Column::value)
                         .filter(Predicate.not(String::isBlank))
                         .orElse(field.getName());
@@ -33,16 +37,52 @@ public class ReflectionMapper implements Mapper{
         return map;
     }
 
-    private static <T> Object getValue(T entity, Field field)  {
+    @Override
+    public <T> T toEntity(Map<String, Object> map) {
+        Objects.requireNonNull(map, "map is required");
+        T entity = getEntity(map.get(ENTITY_ENTRY).toString());
+        Class<?> type = entity.getClass();
+        for (Field field : type.getDeclaredFields()) {
+            String key = Optional.ofNullable(field.getAnnotation(Column.class))
+                    .map(Column::value)
+                    .filter(Predicate.not(String::isBlank))
+                    .orElse(field.getName());
+            Optional<Object> value = Optional.ofNullable(map.get(key));
+            value.ifPresent(v -> setValue(entity, field, v));
+        }
+
+        return entity;
+    }
+
+    private <T> void setValue(T entity, Field field, Object value) {
+        field.setAccessible(true);
         try {
-            return field.get(entity);
+            field.set(entity, value);
         } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         }
     }
 
-    @Override
-    public <T> T toEntity(Map<String, Object> map) {
-        return null;
+    private <T> T getEntity(String entity) {
+        try {
+            Class<?> type = Class.forName(entity);
+            for (Constructor<?> constructor : type.getConstructors()) {
+                if (constructor.getParameterCount() == 0) {
+                    return (T) constructor.newInstance();
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        throw new RuntimeException("it should have a constructor");
+    }
+
+
+    private static <T> Object getValue(T entity, Field field) {
+        try {
+            return field.get(entity);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
