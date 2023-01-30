16174b4882adb5f5d19b11ee29c9cfeddee7fdda
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-30 11:05:52 +0000

    create mapper repository
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/05-reflections/src/main/java/expert/os/examples/mapping/MapperInvocationHandler.java
+++ 2023/05-reflections/src/main/java/expert/os/examples/mapping/MapperInvocationHandler.java
@@ -1,2 +1,30 @@
-package expert.os.examples.mapping;public class MapperInvocationHandler {
+package expert.os.examples.mapping;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+final class MapperInvocationHandler implements InvocationHandler {
+
+    private Mapper mapper = new ReflectionMapper();
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        String name = method.getName();
+
+        switch (name) {
+            case "entity":
+                Map<String, Object> maps = (Map<String, Object>) args[0];
+                Objects.requireNonNull(maps, "maps is required");
+                return mapper.toEntity(maps);
+            case "map":
+                Object entity = args[0];
+                Objects.requireNonNull(entity, "entity is required");
+                return mapper.toMap(entity);
+        }
+      throw new RuntimeException("");
+    }
 }
