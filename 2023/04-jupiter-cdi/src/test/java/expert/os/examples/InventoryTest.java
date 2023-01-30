6f855cbba5a528ef8eecc56ee20edd6609b4d2c7
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-30 09:28:47 +0000

    update inventory tes
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/04-jupiter-cdi/src/test/java/expert/os/examples/InventoryTest.java
+++ 2023/04-jupiter-cdi/src/test/java/expert/os/examples/InventoryTest.java
@@ -1,26 +1,17 @@
 package expert.os.examples;
 
-import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Inject;
 import org.jboss.weld.junit5.auto.AddPackages;
-import org.jboss.weld.junit5.auto.EnableAlternatives;
 import org.jboss.weld.junit5.auto.EnableAutoWeld;
-import org.jboss.weld.junit5.auto.ExcludeBean;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @EnableAutoWeld
 @AddPackages(Inventory.class)
-@EnableAlternatives(PaymentMock.class)
+@AddPackages(PaymentMock.class)
 class InventoryTest {
 
     @Inject
     private Inventory inventory;
-
-    @Produces
-    @ExcludeBean
-    private Payment payment = new PaymentMock();
 
     @Test
     public void shouldBy() {
