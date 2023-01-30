a2f614c64bf15ea421a4baa0b3aaed05b4088431
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-30 11:06:54 +0000

    create mapperrepository factory
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/05-reflections/src/main/java/expert/os/examples/mapping/MapperRepository.java
+++ 2023/05-reflections/src/main/java/expert/os/examples/mapping/MapperRepository.java
@@ -1,9 +1,17 @@
 package expert.os.examples.mapping;
 
+import java.lang.reflect.Proxy;
 import java.util.Map;
 
 public interface MapperRepository {
 
     <T> T entity(Map<String, Object> map);
     <T> Map<String, Object>  map(T entity);
+
+    static MapperRepository repository() {
+        return (MapperRepository) Proxy.newProxyInstance(
+                MapperRepository.class.getClassLoader(),
+                new Class[] { Map.class },
+                new MapperInvocationHandler());
+    }
 }
