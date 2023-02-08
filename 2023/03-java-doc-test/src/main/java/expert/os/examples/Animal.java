f70e6e1e4757bcca2eea9136ae696b40acad73dc
Otavio Santana <otaviopolianasantana@gmail.com>
2023-02-08 08:06:09 +0000

    create animal struture to a don't do item
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ 2023/03-java-doc-test/src/main/java/expert/os/examples/Animal.java
@@ -0,0 +1,31 @@
+package expert.os.examples;
+
+
+/**
+ * <b>Don't do this</b>
+ * This is a class
+ */
+public class Animal {
+
+    /**
+     * This is a field
+     */
+    private String name;
+
+    /**
+     * This is aconstructor
+     * @param name
+     */
+    public Animal(String name) {
+        this.name = name;
+    }
+
+
+    /**
+     * This is a getter
+     * @return
+     */
+    public String name() {
+        return name;
+    }
+}
