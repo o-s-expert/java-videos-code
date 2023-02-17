f80c3ac8a494a54c9e4d7dfe395e60200232ddfd
Otavio Santana <otaviopolianasantana@gmail.com>
2023-02-17 16:55:41 +0000

    add a ride info
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/03-java-doc-test/src/main/java/expert/os/workshop/ddd/taxi/Ride.java
+++ 2023/03-java-doc-test/src/main/java/expert/os/workshop/ddd/taxi/Ride.java
@@ -1,6 +1,16 @@
 package expert.os.workshop.ddd.taxi;
 
 import java.time.LocalTime;
+import java.util.Objects;
 
-public record Ride (String departure, String destination, int distance, LocalTime time) {
+public record Ride(String departure, String destination, int distance, LocalTime time) {
+
+    public Ride {
+        Objects.requireNonNull(departure, "departure is required");
+        Objects.requireNonNull(destination, "departure is required");
+        Objects.requireNonNull(time, "departure is required");
+        if (distance < 0) {
+            throw new IllegalArgumentException("A distance cannot be negative");
+        }
+    }
 }
