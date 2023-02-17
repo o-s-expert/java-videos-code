809621bf221160778e76adcc249524f62ab67a22
Otavio Santana <otaviopolianasantana@gmail.com>
2023-02-17 16:07:04 +0000

    remove unused classes
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/03-java-doc-test/src/main/java/expert/os/workshop/ddd/ChampionRepository.java
+++
@@ -1,7 +0,0 @@
-package expert.os.workshop.ddd;
-
-public interface ChampionRepository {
-    boolean existByName(String name);
-
-    void save(Team team);
-}
--- 2023/03-java-doc-test/src/main/java/expert/os/workshop/ddd/OverTeamException.java
+++
@@ -1,7 +0,0 @@
-package expert.os.workshop.ddd;
-
-public class OverTeamException extends RuntimeException{
-    public OverTeamException(String message) {
-        super(message);
-    }
-}
