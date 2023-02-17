e12876f4369aaa19e08b4e11d4b0c2347059dbca
Otavio Santana <otaviopolianasantana@gmail.com>
2023-02-17 17:13:01 +0000

    add taximeter
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/03-java-doc-test/src/main/java/expert/os/workshop/ddd/taxi/Taximeter.java
+++ 2023/03-java-doc-test/src/main/java/expert/os/workshop/ddd/taxi/Taximeter.java
@@ -1,12 +1,35 @@
 package expert.os.workshop.ddd.taxi;
 
+import org.javamoney.moneta.Money;
+
+import javax.money.CurrencyUnit;
+import javax.money.Monetary;
 import javax.money.MonetaryAmount;
+import java.time.LocalTime;
+import java.util.Locale;
+import java.util.Objects;
 
 public class Taximeter {
 
+    private static final LocalTime START_ECONOMIC = LocalTime.of(8, 0, 0);
+    private static final LocalTime END_ECONOMIC = LocalTime.of(8, 0, 0);
+
+    private static final CurrencyUnit USD = Monetary.getCurrency(Locale.US);
+    private static final MonetaryAmount ECONOMIC = Money.of(1, USD);
+    private static final MonetaryAmount EXPENSIVE = Money.of(2, USD);
 
     public MonetaryAmount fare(Ride ride) {
-        throw new IllegalArgumentException();
+        Objects.requireNonNull(ride, "ride is required");
+        MonetaryAmount flag = flag(ride.time());
+        return flag.multiply(ride.distance());
+    }
+
+
+    private MonetaryAmount flag(LocalTime time) {
+        if (START_ECONOMIC.isAfter(time) && END_ECONOMIC.isAfter(END_ECONOMIC)) {
+            return ECONOMIC;
+        }
+        return EXPENSIVE;
     }
 
 }
