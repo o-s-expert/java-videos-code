23bfee96923b2fdb9266e0a8e2c58c88e3f9d675
Otavio Santana <otaviopolianasantana@gmail.com>
2023-04-13 10:11:42 +0100

    chore: create junit scene one
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/06-java-functions/src/test/java/expert/os/videos/demo/ConsumerTest.java
+++ 2023/06-java-functions/src/test/java/expert/os/videos/demo/ConsumerTest.java
@@ -1,5 +1,6 @@
 package expert.os.videos.demo;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.util.function.Consumer;
@@ -7,12 +8,14 @@ import java.util.function.Consumer;
 public class ConsumerTest {
 
     @Test
+    @Tag("consumer")
     public void shouldCreateConsumer() {
         Consumer<String> log = (s) -> System.out.println("my log: " + s);
         log.accept("Ada is my dog");
     }
 
     @Test
+    @Tag("consumer")
     public void shouldCombineConsumer() {
         Consumer<String> logA = (s) -> System.out.println("[A] my log: " + s);
         Consumer<String> logB = (s) -> System.out.println("[B] my log: " + s);
