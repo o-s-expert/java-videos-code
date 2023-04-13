f2ab739ee3c19ce520dd0bf094b18ba676c63459
Otavio Santana <otaviopolianasantana@gmail.com>
2023-04-13 10:19:19 +0100

    chore: final act on test video
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/06-java-functions/src/test/java/expert/os/videos/demo/SupplierTest.java
+++ 2023/06-java-functions/src/test/java/expert/os/videos/demo/SupplierTest.java
@@ -2,10 +2,12 @@ package expert.os.videos.demo;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
 import java.util.Optional;
 import java.util.function.Supplier;
 
+@EnabledIfSystemProperty(named = "os.exepert.test",matches = "true")
 public class SupplierTest {
 
     @Test
