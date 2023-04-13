2a66546e99d42a92bf80eb518cfb1684633bea69
Otavio Santana <otaviopolianasantana@gmail.com>
2023-04-13 11:13:09 +0100

    chore: remove several dependencies
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/08-java-design/src/main/java/expert/os/videos/demo/Player.java
+++
@@ -1,9 +0,0 @@
-package expert.os.videos.demo;
-
-public record Player(String name, String city, int score) implements Comparable<Player> {
-
-    @Override
-    public int compareTo(Player player) {
-        return this.name.compareTo(player.name);
-    }
-}
