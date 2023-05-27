a55cb1f4ea0aceb7e12528e8b909fa126c3f35bd
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-27 20:23:27 +0100

    feat: create default method
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/05-reflections/src/main/java/expert/os/examples/mapping/MapperRepository.java
+++ 2023/05-reflections/src/main/java/expert/os/examples/mapping/MapperRepository.java
@@ -9,6 +9,11 @@ public interface MapperRepository {
 
     <T> Map<String, Object> map(T entity);
 
+    default <T> T wraper(Map<String, Object> map) {
+        map.put("version", 1);
+        return this.entity(map);
+    }
+
     static MapperRepository repository() {
         return (MapperRepository) Proxy.newProxyInstance(MapperRepository.class.getClassLoader(),
                 new Class[]{MapperRepository.class}, new MapperInvocationHandler());
