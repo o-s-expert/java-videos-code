0ce366cfd42be5985a2627f8b9a3fd3ffe1c73b8
Otavio Santana <otaviopolianasantana@gmail.com>
2023-09-28 09:31:16 +0100

    refact: rename test name
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2023/12-java-21/src/test/java/expert/os/examples/VirtualThreadsTest.java
+++ 2023/12-java-21/src/test/java/expert/os/examples/VirtualThreadsTest.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class VirtualThreads {
+public class VirtualThreadsTest {
 
     @Test
     public void shouldTest() throws InterruptedException {
