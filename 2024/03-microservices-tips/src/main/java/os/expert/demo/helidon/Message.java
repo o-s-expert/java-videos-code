49346370d8faa6b7c8c1aef5489d43d5c9d1cb15
Otavio Santana <otaviopolianasantana@gmail.com>
2024-08-10 19:05:19 +0100

    chore: include initial project
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ 2024/03-microservices-tips/src/main/java/os/expert/demo/helidon/Message.java
@@ -0,0 +1,31 @@
+package os.expert.demo.helidon;
+
+public class Message {
+
+    private String message;
+
+    private String greeting;
+
+    public Message() {
+    }
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getGreeting() {
+        return this.greeting;
+    }
+}
