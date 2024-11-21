beb1a1cd722f31e4168bd81c4c0b69b01db170bc
Otavio Santana <otaviopolianasantana@gmail.com>
2024-11-21 20:05:42 +0000

    feat: create credit card entity sample
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2024/05-jmolecules/src/main/java/expert/os/examples/CreditCard.java
+++ 2024/05-jmolecules/src/main/java/expert/os/examples/CreditCard.java
@@ -1,11 +1,20 @@
 package expert.os.examples;
 
 import org.jmolecules.ddd.annotation.Entity;
+import org.jmolecules.ddd.annotation.Identity;
+
+import java.math.BigInteger;
+import java.time.YearMonth;
 
 @Entity
 public class CreditCard {
 
+    @Identity
+    private BigInteger id;
+
     private String number;
 
     private String name;
+
+    private YearMonth expiry;
 }
