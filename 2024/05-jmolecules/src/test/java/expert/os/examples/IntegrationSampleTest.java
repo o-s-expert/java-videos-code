136b638acf83d56252018f97d1396a2023998278
Otavio Santana <otaviopolianasantana@gmail.com>
2024-11-21 19:42:43 +0000

    chore: remove code
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ 2024/05-jmolecules/src/test/java/expert/os/examples/IntegrationSampleTest.java
@@ -0,0 +1,27 @@
+package expert.os.examples;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.EvaluationResult;
+import org.jmolecules.archunit.JMoleculesArchitectureRules;
+import org.jmolecules.archunit.JMoleculesDddRules;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@AnalyzeClasses(packages = "example")
+public class IntegrationSampleTest {
+
+    @ArchTest
+    private ArchRule dddRules = JMoleculesDddRules.all();
+    @ArchTest
+    private ArchRule layering = JMoleculesArchitectureRules.ensureLayering();
+
+    @ArchTest
+    void detectsViolations(JavaClasses classes) {
+
+        EvaluationResult result = JMoleculesDddRules.all().evaluate(classes);
+        assertThat(result.hasViolation()).isFalse();
+    }
+}
