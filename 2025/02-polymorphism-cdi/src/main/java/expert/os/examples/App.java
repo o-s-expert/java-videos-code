db1cf465b9556564e0201e4d5df5d3766237f6c2
Otavio Santana <otaviopolianasantana@gmail.com>
2025-03-31 08:25:03 +0100

    feat: define sample code using main
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 2025/02-polymorphism-cdi/src/main/java/expert/os/examples/App.java
+++ 2025/02-polymorphism-cdi/src/main/java/expert/os/examples/App.java
@@ -8,6 +8,14 @@ public class App {
     public static void main(String[] args) {
         try(SeContainer container = SeContainerInitializer.newInstance().initialize()) {
             Orchestra orchestra = container.select(Orchestra.class).get();
+
+            Instrument piano = orchestra.playInstrument(InstrumentType.KEYBOARD);
+            System.out.println("Playing: " + piano.sound());
+            Instrument violin = orchestra.playInstrument(InstrumentType.STRING);
+            System.out.println("Playing: " + violin.sound());
+            Instrument xylophone = orchestra.playInstrument(InstrumentType.PERCUSSION);
+            System.out.println("Playing: " + xylophone.sound());
+
         }
     }
 }
--- 2025/02-polymorphism-cdi/src/main/java/expert/os/examples/Orchestra.java
+++ 2025/02-polymorphism-cdi/src/main/java/expert/os/examples/Orchestra.java
@@ -1,7 +1,25 @@
 package expert.os.examples;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Any;
+import jakarta.enterprise.inject.Instance;
+import jakarta.inject.Inject;
+
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class Orchestra {
+
+    private static final Logger LOGGER = Logger.getLogger(Orchestra.class.getName());
+
+    @Inject
+    @Any
+    private Instance<Instrument> instruments;
+
+    public Instrument playInstrument(InstrumentType type) {
+        LOGGER.fine("Playing instrument: " + type);
+        return instruments.select(MusicalInstrumentLiteral.of(type))
+                .get();
+    }
+
 }
