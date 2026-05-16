a8ee198898a85ffd73ac58bff878d91b50ac06ec
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-16 11:09:46 +0100

    chore: add TransferService with player transfer logic

---
+++ 2026/02-ddd/src/main/java/expert/os/videos/soccer/TransferService.java
@@ -0,0 +1,13 @@
+package expert.os.videos.soccer;
+
+public class TransferService {
+
+    public void transfer(
+            Player player,
+            Team source,
+            Team destination) {
+
+        source.remove(player);
+        destination.addPlayer(player);
+    }
+}
