1ddac9e0c251ace02b16999167f07dcca4818088
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-16 11:04:25 +0100

    chore: add Team entity with id, name, and players fields

---
+++ 2026/02-ddd/src/main/java/expert/os/videos/soccer/Team.java
@@ -0,0 +1,14 @@
+package expert.os.videos.soccer;
+
+import java.util.List;
+import java.util.UUID;
+
+public class Team {
+
+    private UUID id;
+
+    private String name;
+
+    private List<Player> players;
+
+}
