398869c9053c55023ee18cda46a83444b1991869
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-25 07:36:23 +0100

    remove soccer package

--- 2026/03-helo-world-ai/src/main/java/expert/os/videos/soccer/EventPublisher.java
+++
@@ -1,6 +0,0 @@
-package expert.os.videos.soccer;
-
-public interface EventPublisher {
-
-    void publish(Object event);
-}
--- 2026/03-helo-world-ai/src/main/java/expert/os/videos/soccer/NewSoccerHired.java
+++
@@ -1,4 +0,0 @@
-package expert.os.videos.soccer;
-
-public record NewSoccerHired (Team team, Player player){
-}
