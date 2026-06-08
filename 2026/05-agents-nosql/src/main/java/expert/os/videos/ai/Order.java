8fa63b08a4af1d6621abb2fc4870b5c20058441e
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-08 08:15:16 +0100

    feat: add Order entity with methods for creation and cancellation

---
+++ 2026/05-agents-nosql/src/main/java/expert/os/videos/ai/Order.java
@@ -0,0 +1,20 @@
+package expert.os.videos.ai;
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+
+import java.util.UUID;
+
+@Entity
+public record Order(@Id UUID id, @Column UUID customerId, @Column String product, @Column OrderStatus status) {
+
+
+    public static Order of(User user, String product) {
+        return new Order(UUID.randomUUID(), user.getId(), product, OrderStatus.PLACED);
+    }
+
+    public Order cancel() {
+        return new Order(id, customerId, product, OrderStatus.CANCELLED);
+    }
+}
