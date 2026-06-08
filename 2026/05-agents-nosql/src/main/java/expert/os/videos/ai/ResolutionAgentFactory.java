dd979ae53080e62fdd03cbe3184378340f50b805
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-08 08:26:30 +0100

    chore: bump JNoSQL version to 1.1.14

--- 2026/05-agents-nosql/src/main/java/expert/os/videos/ai/ResolutionAgentFactory.java
+++ 2026/05-agents-nosql/src/main/java/expert/os/videos/ai/ResolutionAgentFactory.java
@@ -1,26 +1,32 @@
 package expert.os.videos.ai;
 
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.service.AiServices;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Inject;
-
 
 @ApplicationScoped
 public class ResolutionAgentFactory {
 
-    @Inject
-    private ChatModel chatModel;
+    private final ChatModel chatModel;
+
+    private final  OrderService orderService;
+
+    private final  UserService userService;
 
     @Inject
-    private EnterpriseTools tools;
+    public ResolutionAgentFactory(ChatModel chatModel, OrderService orderService, UserService userService) {
+        this.chatModel = chatModel;
+        this.orderService = orderService;
+        this.userService = userService;
+    }
 
-    @Produces
-    public CustomerResolutionAgent create() {
+    public CustomerResolutionAgent createAgent() {
         return AiServices.builder(CustomerResolutionAgent.class)
                 .chatModel(chatModel)
-                .tools(tools)
+                .chatMemory(MessageWindowChatMemory.withMaxMessages(20))
+                .tools(orderService, userService) // Injecting the suite of tools
                 .build();
     }
 }
