7ba8d4a4b1e9b659872d592f1eb8191ea9236a16
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-21 10:17:38 +0100

    chore: add booking details fields and constructor to FlightBooking

--- 2026/06-factory-patterns/src/main/java/expert/os/videos/flight/FlightBooking.java
+++ 2026/06-factory-patterns/src/main/java/expert/os/videos/flight/FlightBooking.java
@@ -1,4 +1,18 @@
 package expert.os.videos.flight;
 
 public class FlightBooking {
+
+    private final String passengerName;
+    private final String departureCity;
+    private final String destinationCity;
+    private final String seatNumber;
+    private final int baggageCount;
+
+    public FlightBooking(String passengerName, String departureCity, String destinationCity, String seatNumber, int baggageCount) {
+        this.passengerName = passengerName;
+        this.departureCity = departureCity;
+        this.destinationCity = destinationCity;
+        this.seatNumber = seatNumber;
+        this.baggageCount = baggageCount;
+    }
 }
